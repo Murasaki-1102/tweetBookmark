1ae6d38cc708884eb569dead6413c91dc256e63c
purpleeeee <h.purpleeeee@gmail.com>
2021-02-22 00:03:14 +0900

    💡 Add EditTheme

--- src/components/atoms/ThemeSwitcher.tsx
+++ src/components/atoms/ThemeSwitcher.tsx
@@ -1,14 +1,19 @@
-import React, { useContext } from "react";
-import { Toggle, ThemeContext, useTheme } from "react-native-magnus";
-
+import React from "react";
+import { Toggle, ToggleProps, useTheme } from "react-native-magnus";
 import { themes } from "../../constants/theme";
 
-export const ThemeSwitcher = () => {
+export const ThemeSwitcher = (props?: Omit<ToggleProps, "onPress">) => {
   const { theme, setTheme } = useTheme();
-  console.log(theme);
   const onToggle = () => {
     theme.name === "dark" ? setTheme(themes.light) : setTheme(themes.dark);
   };
 
-  return <Toggle on={theme.name === "dark"} onPress={onToggle} />;
+  return (
+    <Toggle
+      on={theme.name === "dark"}
+      onPress={onToggle}
+      activeBg="gray700"
+      {...props}
+    />
+  );
 };
--- src/components/screens/EditThemeScreen.tsx
+++ src/components/screens/EditThemeScreen.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { Div } from "react-native-magnus";
+import { Div, Button, Text } from "react-native-magnus";
 import { ThemeSwitcher } from "../atoms/ThemeSwitcher";
 
 export const EditThemeScreen = () => {
   return (
     <SafeAreaView>
       <Div h="100%">
-        <ThemeSwitcher />
+        <Button
+          block
+          rounded="none"
+          borderBottomWidth={1}
+          borderColor="selected"
+          suffix={<ThemeSwitcher />}
+        >
+          <Text fontSize="2xl" w="85%">
+            ダークテーマに変更する
+          </Text>
+        </Button>
       </Div>
     </SafeAreaView>
   );
