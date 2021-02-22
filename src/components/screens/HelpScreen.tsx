f79be2797c7b5d1187ef702c0131a01c6f5bffdc
purpleeeee <h.purpleeeee@gmail.com>
2021-02-22 21:27:39 +0900

    ✏️

--- src/components/screens/HelpScreen.tsx
+++ src/components/screens/HelpScreen.tsx
@@ -5,7 +5,10 @@ import { WebView } from "react-native-webview";
 export const HelpScreen: FC = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView source={{ uri: "https://forms.gle/w58k3y5pAhE9npBg8" }} />
+      <WebView
+        source={{ uri: "https://forms.gle/w58k3y5pAhE9npBg8" }}
+        startInLoadingState
+      />
     </SafeAreaView>
   );
 };
